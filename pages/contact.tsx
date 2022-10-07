2d512f01637aa8ccbf76bb0737d0a17dd61afb44
Taiming Man <mantaiming@gmail.com>
2022-10-07 16:39:06 +0800

    fix: fix typo

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,7 +5,7 @@ import Input from '../components/input';
 
 export default function Concat() {
   return <Container>
-    <Content title='Concats Us' disableButton>
+    <Content title='Contact Us' disableButton>
       <div className='grid justify-between grid-cols-1 lg:grid-cols-2 gap-7.5'>
         <div className='flex flex-col gap-[16px] lg:gap-[4px] xl:gap-[16px] order-2 lg:order-1 w-full'>
           <Input title='Name' placeholder='Your name here'/>
